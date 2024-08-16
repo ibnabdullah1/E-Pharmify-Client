0dd6f3a327e0a9d0a17493c39589ce4613f7960f
ibnabdullah1 <arfathossen541@gmail.com>
2024-08-16 23:40:11 +0600

    baseURL path difined

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,33 +1,44 @@
 "use client";
 import Loader from "@/components/Common/Loader";
-import StoreProvider from "@/lib/Provider/StoreProvider";
+import { selectCurrentUser } from "@/redux/features/auth/authSlice";
+import { RootState } from "@/redux/features/store";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { Toaster } from "react-hot-toast";
+import { useSelector } from "react-redux";
 import Footer from "../../components/Home/Footer/Footer";
 import Header from "../../components/Home/NavBar/Header";
 import Navbar from "../../components/Navbar/Navbar";
-import "./globals.css";
 
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
+  const user = useSelector((state: RootState) => selectCurrentUser(state));
+  const router = useRouter();
   const [loading, setLoading] = useState<boolean>(true);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    if (user === null) {
+      router.push("/login");
+    } else {
+      setIsAuthenticated(true);
+    }
+  }, [user, router]);
 
   useEffect(() => {
     const timer = setTimeout(() => setLoading(false), 3000);
     return () => clearTimeout(timer);
   }, []);
 
+  if (isAuthenticated === null) {
+    return null;
+  }
+
   return (
-    <html lang="en">
-      <StoreProvider>
-        <body>
-          <Toaster position="top-center" reverseOrder={false} />
-          <Header />
-          <Navbar />
-          <div className="px-5 lg:px-0">{loading ? <Loader /> : children}</div>
-          <Footer />
-        </body>
-      </StoreProvider>
-    </html>
+    <>
+      <Header />
+      <Navbar />
+      <div className="px-5 lg:px-0">{loading ? <Loader /> : children}</div>
+      <Footer />
+    </>
   );
 };
 
